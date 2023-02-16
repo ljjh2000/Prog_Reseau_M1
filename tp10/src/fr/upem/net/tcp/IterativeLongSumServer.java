edeac6edd5383bb7b44fea00dcef09db59d95964
ljjh2000 <jacqueline93140@gmail.com>
2023-02-16 12:45:03 +0100

    add tp10 exo1

---
+++ tp10/src/fr/upem/net/tcp/IterativeLongSumServer.java
@@ -0,0 +1,114 @@
+package fr.upem.net.tcp;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class IterativeLongSumServer {
+
+  private static final Logger logger = Logger.getLogger(IterativeLongSumServer.class.getName());
+  private static final int BUFFER_SIZE = 1024;
+  private final ServerSocketChannel serverSocketChannel;
+
+  private final ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+
+  public IterativeLongSumServer(int port) throws IOException {
+    serverSocketChannel = ServerSocketChannel.open();
+    serverSocketChannel.bind(new InetSocketAddress(port));
+    logger.info(this.getClass().getName() + " starts on port " + port);
+  }
+
+  /**
+   * Iterative server main loop
+   *
+   * @throws IOException
+   */
+
+  public void launch() throws IOException {
+    logger.info("Server started");
+    while (!Thread.interrupted()) {
+      SocketChannel client = serverSocketChannel.accept();
+      try {
+        logger.info("Connection accepted from " + client.getRemoteAddress());
+        serve(client);
+      } catch (IOException ioe) {
+        logger.log(Level.SEVERE, "Connection terminated with client by IOException", ioe.getCause());
+      } finally {
+        silentlyClose(client);
+      }
+    }
+  }
+
+  /**
+   * Treat the connection sc applying the protocol. All IOException are thrown
+   *
+   * @param sc
+   * @throws IOException
+   */
+  private void serve(SocketChannel sc) throws IOException {
+
+    while (true) {
+      buffer.clear();
+      buffer.limit(Integer.BYTES);
+      if (!readFully(sc, buffer)){
+        return;
+      }
+      buffer.flip();
+      var nb = buffer.getInt();
+      buffer.clear();
+      buffer.limit(nb * Long.BYTES);
+      if (!readFully(sc, buffer)){
+        return;
+      }
+      buffer.flip();
+      long sum = 0L;
+      while (buffer.hasRemaining()){
+        sum += buffer.getLong();
+      }
+
+      logger.info("send " + sum);
+
+      buffer.clear();
+      buffer.putLong(sum);
+      buffer.flip();
+
+      sc.write(buffer);
+    }
+  }
+
+  /**
+   * Close a SocketChannel while ignoring IOExecption
+   *
+   * @param sc
+   */
+
+  private void silentlyClose(Closeable sc) {
+    if (sc != null) {
+      try {
+        sc.close();
+      } catch (IOException e) {
+        // Do nothing
+      }
+    }
+  }
+
+  static boolean readFully(SocketChannel sc, ByteBuffer buffer) throws IOException {
+    while (buffer.hasRemaining()) {
+      if (sc.read(buffer) == -1) {
+        logger.info("Input stream closed");
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    var server = new IterativeLongSumServer(Integer.parseInt(args[0]));
+    server.launch();
+  }
+}
